95c495456b928ab79a4ee16d7188b6393a8bebfa
siro-Git-Hub627628 <yuutokasai6@icloud.com>
2026-08-05 11:23:32 +0900

    操作方法の画像サイズを最適化

--- Game/Controller.cpp
+++ Game/Controller.cpp
@@ -5,7 +5,7 @@
 #include "sound/SoundEngine.h"
 Controller::Controller()
 {
-	spriteRender.Init("Assets/sprite/controller.dds", 1700.0f, 1080.0f);
+	spriteRender.Init("Assets/sprite/controller.dds", 1920.0f, 1080.0f);
 	NextRender.SetPosition({ 350.0f,515.0f,0.0f });
 	NextRender.SetColor(g_vec4Black);
 	NextRender.SetText(L"NEXT-ゲーム説明");
